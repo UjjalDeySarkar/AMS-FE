bebed409d1947d969767872d5e49bff32748160b
ujjal <ujjaldeysarkar123@gmail.com>
2025-08-05 18:24:03 +0530

    Update API_URL in config to use ngrok endpoint

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 export const config = {
-    API_URL: 'http://localhost:8080',  // You can change this URL when needed
+    API_URL: 'https://1a09fd99dcff.ngrok-free.app',  // You can change this URL when needed
     headers: {
         'ngrok-skip-browser-warning': 'true'
     }
