20ea434172602ecef3b90728083434740c8f0a4f
ujjal <ujjaldeysarkar123@gmail.com>
2025-09-02 16:45:21 +0530

    Update API_URL in config to point to the new server address

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 export const config = {
-    API_URL: 'https://1a09fd99dcff.ngrok-free.app',  // You can change this URL when needed
+    API_URL: 'http://13.233.161.237',  // You can change this URL when needed
     headers: {
         'ngrok-skip-browser-warning': 'true'
     }
